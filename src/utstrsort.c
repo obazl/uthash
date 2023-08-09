a0995d04c726897e94a6d49164b6ec40e19d6a37
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2023-08-09 18:31:07 -0500

    initial bzlmod commit

---
+++ src/utstrsort.c
@@ -0,0 +1,10 @@
+#include <string.h>
+#include "utstrsort.h"
+
+int strsort(const void *_a, const void *_b)
+{
+    const char *a = *(const char* const *)_a;
+    const char *b = *(const char* const *)_b;
+    return strcmp(a,b);
+}
+
---
+++ src/utstrsort.h
@@ -0,0 +1,6 @@
+#ifndef UTSTRSORT_H
+#define UTSTRSORT_H
+
+int strsort(const void *_a, const void *_b);
+
+#endif
